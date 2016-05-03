8b560ad823297ed9a188fd0928533e0d6b677012
Masashi Fujita <objectxtreme@gmail.com>
2016-05-03 19:41:16 +0900

    feat: Implement handle based I/O

---
+++ include/lowio/lowio.hpp
@@ -0,0 +1,342 @@
+/*
+ * lowio.h: Performs I/O via lowlevel system dependent API
+ *
+ * AUTHOR(S): objectx
+ */
+#pragma once
+
+#ifndef	lowio_hpp__96145883_4CE0_42BE_83E5_CD4D75A540A0
+#define	lowio_hpp__96145883_4CE0_42BE_83E5_CD4D75A540A0 1
+
+#include <sys/types.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include <stdexcept>
+#include <string>
+
+namespace LowIO {
+
+    enum class ErrorCode { OPEN_FAILED
+                         , CREATE_FAILED
+                         , CLOSE_FAILED
+                         , BAD_HANDLE
+                         , READ_FAILED
+                         , WRITE_FAILED
+                         , SEEK_FAILED
+                         , TRUNCATE_FAILED
+                         , DUPLICATE_FAILED } ;
+
+    class Error final : public std::runtime_error {
+    private:
+        ErrorCode	code_ ;
+    public:
+        Error (ErrorCode code, const std::string &msg)
+                : std::runtime_error { msg }, code_ { code } {
+            /* NO-OP */
+        }
+
+        ErrorCode   code () const {
+            return code_ ;
+        }
+    } ;
+
+    enum class SeekOrigin { BEGIN, END, CURRENT };
+
+    // FileHandle aliases...
+#if defined (_WIN32) || defined (_WIN64)
+    using handle_t = HANDLE ;
+    const handle_t	BAD_HANDLE = INVALID_HANDLE_VALUE ;
+    inline bool	valid_handle_p (handle_t H) {
+        return (H != BAD_HANDLE) ;
+    }
+#else
+    using handle_t = int ;
+    const handle_t	BAD_HANDLE = static_cast<handle_t> (-1) ;
+
+    inline bool	valid_handle_p (handle_t H) {
+        return (0 <= H) ;
+    }
+#endif
+
+    /**
+     * Gets OS's native handle for input.
+     *
+     * @return handle for input
+     */
+    inline handle_t	GetSTDIN () {
+#if defined (_WIN32) || defined (_WIN64)
+        return GetStdHandle (STD_INPUT_HANDLE) ;
+#else	/* not (_WIN32 || _WIN64) */
+        return 0 ;
+#endif	/* not (_WIN32 || _WIN64) */
+    }
+
+    /**
+     * Gets OS's native handle for output.
+     *
+     * @return handle for output
+     */
+    inline handle_t	GetSTDOUT () {
+#if defined (_WIN32) || defined (_WIN64)
+        return GetStdHandle (STD_OUTPUT_HANDLE) ;
+#else	/* not (_WIN32 || _WIN64) */
+        return 1 ;
+#endif	/* not (_WIN32 || _WIN64) */
+    }
+
+    /**
+     * Gets OS's native handle for error output.
+     *
+     * @return handle for error output
+     */
+    inline handle_t	GetSTDERR () {
+#if defined (_WIN32) || defined (_WIN64)
+        return GetStdHandle (STD_ERROR_HANDLE) ;
+#else	/* not (_WIN32 || _WIN64) */
+        return 2 ;
+#endif	/* not (_WIN32 || _WIN64) */
+    }
+
+    /** Wrapper class for using OS's native file handle.  */
+    class _os_handle_t final {
+    private:
+        handle_t	value_ ;
+    public:
+        ~_os_handle_t () ;
+
+        _os_handle_t () : value_ { BAD_HANDLE } {/* NO-OP */}
+
+        explicit _os_handle_t (handle_t h) : value_ { h } {/* NO-OP */}
+
+        _os_handle_t (_os_handle_t &&src) : value_ { src.detach () } { /* NO-OP */ }
+
+        operator handle_t () const {
+            return value_ ;
+        }
+
+        _os_handle_t &	operator = (_os_handle_t &&src) {
+            attach (src.detach ()) ;
+            return *this ;
+        }
+
+        handle_t	handle () const {
+            return value_ ;
+        }
+
+        bool		valid () const {
+            return valid_handle_p (value_) ;
+        }
+
+        operator bool () const {
+            return valid () ;
+        }
+
+        bool operator ! () const {
+            return ! valid () ;
+        }
+
+        void		close () ;
+
+        void		attach (handle_t h) {
+            if (valid ()) {
+                this->close () ;
+            }
+            value_ = h ;
+        }
+
+        handle_t	detach () {
+            handle_t	result = value_ ;
+            value_ = BAD_HANDLE ;
+            return result ;
+        }
+
+        /**
+         * Moving file-pointer to specified position.
+         *
+         * @return byte offset from the beginning
+         * @param offset delta value
+         * @param origin origin for computing file-pointer
+         */
+        size_t	seek (int64_t offset, SeekOrigin origin) ;
+
+        /**
+         * Reads <code>size</code> bytes data into <code>data</code>
+         *
+         * @param data buffer to store data
+         * @param size buffer size
+         * @param sz_read actually read bytes
+         */
+        size_t  read (void *data, size_t maxsize) ;
+
+        /**
+         * Writes <code>size</code> bytes data from <code>data</code>.
+         *
+         * @param data the source
+         * @param size size to write
+         */
+        void    write (const void *data, size_t size) ;
+
+        /**
+         * Truncates file at current position.
+         */
+        void    truncate () ;
+
+        /**
+         * Duplicates supplied handle.
+         *
+         * @param h the handle to duplicate
+         */
+        void    duplicate (handle_t h) ;
+    } ;
+
+    class Input {
+    private:
+        _os_handle_t	h_;
+    public:
+        ~Input () { /* NO-OP */ }
+        Input () { /* NO-OP */ }
+        /**
+         * Construct & open <code>file</code>
+         *
+         * @param file file to read
+         */
+        Input (const std::string &file) {
+            this->open (file) ;
+        }
+
+        explicit Input (handle_t h) : h_ (h) { /* NO-OP */ }
+
+        Input (Input &&src) : h_ (src.detach ()) {
+            /* NO-OP */
+        }
+        /**
+         * Opens <code>file</code> for reading.
+         *
+         * @param file file to read
+         */
+        Input &	open (const std::string &file);
+
+        /** Closes attached handle.  */
+        void	close() {
+            h_.close();
+        }
+
+        Input & attach (handle_t h) {
+            h_.attach (h);
+            return *this ;
+        }
+
+        handle_t	detach () {
+            return h_.detach ();
+        }
+
+        /**
+         * Reads at most <code>size</code> bytes data into <code>data</code>
+         *
+         * @return # of bytes read
+         * @param data buffer to store data
+         * @param size buffer size
+         */
+        size_t	fetch (void *data, size_t size) {
+            return h_.read (data, size) ;
+        }
+        /**
+         * Reads <code>size</code> bytes data into <code>data</code>.
+         * Raising exception if failed to read exactly <code>size</code> bytes.
+         *
+         * @param data buffer to store data
+         * @param size size to read
+         */
+        void	read (void *data, size_t size) {
+            auto sz_read = h_.read (data, size);
+            if (sz_read != size) {
+                throw Error { ErrorCode::READ_FAILED, std::string { "Premature EOF" } } ;
+            }
+        }
+        /**
+         * Moving file-pointer to specified position (for giant file).
+         *
+         * @return byte offset from the beginning
+         * @param offset delta value
+         * @param origin origin for computing file-pointer
+         */
+        uint64_t	seek (int64_t offset, SeekOrigin origin) {
+            return static_cast<uint64_t> (h_.seek (offset, origin)) ;
+        }
+
+        Input & duplicate (handle_t h) {
+            h_.duplicate (h) ;
+            return *this ;
+        }
+    };
+
+    class Output {
+    private:
+        _os_handle_t	h_ ;
+    public:
+        ~Output () { /* NO-OP */ }
+        Output () { /* NO-OP */ }
+
+        Output (const std::string &file) {
+            this->open (file) ;
+        }
+
+        explicit Output (handle_t h) : h_ { h } { /* NO-OP */ }
+        Output (Output &&src) : h_ { src.detach() } { /* NO-OP */ }
+
+        /**
+         * Opens & Creates <code>file</code> for writing.
+         *
+         * @param file file to read
+         */
+        void	open (const std::string &file) ;
+
+        void	close () {
+            h_.close() ;
+        }
+
+        Output &	attach (handle_t h) {
+            h_.attach(h) ;
+            return *this ;
+        }
+
+        handle_t	detach () {
+            return h_.detach() ;
+        }
+
+        /**
+         * Writes <code>size</code> bytes data from <code>data</code>.
+         *
+         * @param data   the source
+         * @param size   size to write
+         */
+        void	write (const void *data, size_t size) {
+            h_.write(data, size) ;
+        }
+
+        /**
+         * Moving file-pointer to specified position (for giant file).
+         *
+         * @return byte offset from the beginning
+         * @param offset delta value
+         * @param origin origin for computing file-pointer
+         */
+        uint64_t	seek (int64_t offset, SeekOrigin origin) {
+            return static_cast<uint64_t> (h_.seek (offset, origin)) ;
+        }
+
+        Output &	duplicate (handle_t h) {
+            h_.duplicate(h) ;
+            return *this ;
+        }
+
+        /** Truncate file at current position.  */
+        Output &	truncate () {
+            h_.truncate() ;
+            return *this ;
+        }
+    } ;
+}
+
+#endif	/* lowio_hpp__96145883_4CE0_42BE_83E5_CD4D75A540A0 */
---
+++ src/lowio.cpp
@@ -0,0 +1,257 @@
+#ifdef HAVE_CONFIG_HPP
+#   include "config.hpp"
+#endif
+
+#include "lowio/lowio.hpp"
+
+#ifdef HAVE_SYS_STAT_H
+#   include <sys/stat.h>
+#endif
+
+#ifdef HAVE_FCNTL_H
+#   include <fcntl.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+#   include <unistd.h>
+#endif
+
+#if defined (_WIN32) || defined (_WIN64)
+namespace {
+    std::string	get_last_error_message (DWORD code) {
+        void *	msg = nullptr ;
+        FormatMessageA ((FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS),
+                        NULL,
+                        code,
+                        MAKELANGID (LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+                        (LPTSTR)&msg,
+                        0,
+                        NULL) ;
+        std::string	result { static_cast<char *>(msg) } ;
+        LocalFree (msg) ;
+        return result ;
+    }
+
+    std::string	get_last_error_message () {
+        return get_last_error_message (GetLastError ()) ;
+    }
+}
+#endif
+
+namespace LowIO {
+
+    _os_handle_t::~_os_handle_t () {
+        if (! valid ()) {
+            return ;
+        }
+        try {
+            this->close () ;
+        }
+        catch (const Error &) {
+            value_ = BAD_HANDLE ;
+        }
+    }
+
+    void    _os_handle_t::close () {
+        if (! valid ()) {
+            return ;
+        }
+        handle_t  H = value_ ;
+        value_ = BAD_HANDLE ;
+#if defined (_WIN32) || defined (_WIN64)
+        if (! CloseHandle (H)) {
+            throw Error { ErrorCode::CLOSE_FAILED
+                        , std::string { "Close failed (" }.append (get_last_error_message ()).append (").") } ;
+        }
+#else	/* not (_WIN32 || _WIN64) */
+        if (::close (H) != 0) {
+            throw Error { ErrorCode::CLOSE_FAILED, std::string { "Close failed." } } ;
+        }
+#endif	/* not (_WIN32 || _WIN64) */
+    }
+
+    size_t _os_handle_t::seek (int64_t offset, SeekOrigin origin) {
+        if (! valid ()) {
+            throw Error { ErrorCode::BAD_HANDLE, std::string { "Invalid handle for seek." } } ;
+        }
+#if defined (_WIN32) || defined (_WIN64)
+        LARGE_INTEGER	tmp ;
+        tmp.QuadPart = offset ;
+        switch (origin) {
+        case SeekOrigin::BEGIN:
+            tmp.LowPart = ::SetFilePointer (value_, tmp.LowPart, &tmp.HighPart, FILE_BEGIN) ;
+            break ;
+        case SeekOrigin::CURRENT:
+            tmp.LowPart = ::SetFilePointer (value_, tmp.LowPart, &tmp.HighPart, FILE_CURRENT) ;
+            break ;
+        case SeekOrigin::END:
+            tmp.LowPart = ::SetFilePointer (value_, tmp.LowPart, &tmp.HighPart, FILE_END) ;
+            break ;
+        default:
+            /* NOTREACHED */
+            assert (false) ;
+            break ;
+        }
+        DWORD	err = ::GetLastError () ;
+        if (tmp.LowPart == 0xFFFFFFFFu && err != NO_ERROR)
+            throw Error { ErrorCode::SEEK_FAILED
+                        , std::string { "Seek failed." }.append (get_last_error_message (err)).append (").") } ;
+        return static_cast <uint64_t> (tmp.QuadPart) ;
+#else	/* not (_WIN32 || _WIN64) */
+        int64_t   off ;
+        switch (origin) {
+        case SeekOrigin::BEGIN :
+            off = ::lseek (value_, static_cast <int64_t> (offset), SEEK_SET) ;
+            break ;
+        case SeekOrigin::CURRENT:
+            off = ::lseek (value_, static_cast <int64_t> (offset), SEEK_CUR) ;
+            break ;
+        case SeekOrigin::END:
+            off = ::lseek (value_, static_cast <int64_t> (offset), SEEK_END) ;
+            break ;
+        }
+        if (off == -1L) {
+            throw Error { ErrorCode::SEEK_FAILED, "Seek failed." } ;
+        }
+        return static_cast <uint64_t> (off) ;
+#endif	/* not (_WIN32 || _WIN64) */
+    }
+
+
+    size_t  _os_handle_t::read (void *data, size_t size) {
+        if (! valid ()) {
+            throw Error { ErrorCode::BAD_HANDLE, std::string { "Invalid handle for read." } } ;
+        }
+#if defined (_WIN32) || defined (_WIN64)
+        DWORD n_read = 0 ;
+        if (! ReadFile (value_, data, static_cast <DWORD>(size), &n_read, NULL)) {
+            throw Error { ErrorCode::READ_FAILED
+                        , std::string { "Read failed (" }.append (get_last_error_message ()).append (").") } ;
+        }
+        auto    sz_read = static_cast <size_t> (n_read) ;
+#else	/* not (_WIN32 || _WIN64) */
+        long  n = ::read (value_, data, size) ;
+        if (n < 0) {
+            throw Error { ErrorCode::READ_FAILED, std::string { "Read failed." } } ;
+        }
+        auto sz_read = static_cast <size_t> (n) ;
+#endif	/* not (_WIN32 || _WIN64) */
+        return sz_read ;
+    }
+
+    void	_os_handle_t::write (const void *data, size_t size) {
+        if (! valid ()) {
+            throw Error { ErrorCode::BAD_HANDLE, "Invalid handle for write." } ;
+        }
+#if defined (_WIN32) || defined (_WIN64)
+        // Due to kernel restriction, Writing over 64MBytes data to Network drive cause WriteFile failure.
+        const size_t  MAXIMUM_SEGMENT_SIZE = 32 * 1024 * 1024 ;
+
+        DWORD sz_written = 0 ;
+
+        while (sz_written < static_cast<DWORD> (size)) {
+            DWORD sz = static_cast<DWORD> (size) - sz_written ;
+
+            if (MAXIMUM_SEGMENT_SIZE < sz) {
+                sz = MAXIMUM_SEGMENT_SIZE ;
+            }
+            DWORD	cnt = 0 ;
+
+            if (! WriteFile (value_, static_cast<const char *> (data) + sz_written, sz, &cnt, NULL) ||
+                (cnt != sz))
+                throw Error { ErrorCode::WRITE_FAILED
+                            , std::string { "Write failed (" }.append (get_last_error_message ()).append (").") } ;
+            sz_written += sz ;
+        }
+#else	/* not (_WIN32 || _WIN64) */
+        // It's redundant to UN?X like systems...
+        const size_t  MAXIMUM_SEGMENT_SIZE = 32 * 1024 * 1024 ;
+
+        size_t    sz_written = 0 ;
+
+        while (sz_written < size) {
+            size_t    sz = size - sz_written ;
+            if (MAXIMUM_SEGMENT_SIZE < sz) {
+                sz = MAXIMUM_SEGMENT_SIZE ;
+            }
+            long  cnt = ::write (value_, static_cast<const char *> (data) + sz_written, sz) ;
+            if (cnt < 0 || static_cast<size_t> (cnt) != sz) {
+                throw Error { ErrorCode::WRITE_FAILED, std::string { "Write failed." } } ;
+            }
+            sz_written += sz ;
+        }
+#endif	/* not (_WIN32 || _WIN64) */
+    }
+
+    void	_os_handle_t::truncate () {
+        if (! valid ()) {
+            throw Error { ErrorCode::BAD_HANDLE, "Invalid handle for truncate." } ;
+        }
+#if defined (_WIN32) || defined (_WIN64)
+        if (! SetEndOfFile (value_)) {
+            throw Error { ErrorCode::TRUNCATE_FAILED
+                        , std::string { "Truncate failed (" }.append (get_last_error_message ()).append (").") } ;
+        }
+#else	/* not (_WIN32 || _WIN64) */
+        if (::write (value_, "", 0) < 0) {
+            throw Error { ErrorCode::TRUNCATE_FAILED, "Truncate failed." } ;
+        }
+#endif	/* not (_WIN32 || _WIN64) */
+    }
+
+    void	_os_handle_t::duplicate (handle_t h) {
+#if defined (_WIN32) || defined (_WIN64)
+        HANDLE	h_new ;
+        HANDLE	h_proc = GetCurrentProcess () ;
+        if (! DuplicateHandle (h_proc, h, h_proc, &h_new, 0, false, DUPLICATE_SAME_ACCESS)) {
+            throw Error { ErrorCode::DUPLICATE_FAILED
+                        , std::string { "Duplicate handle failed (" }.append (get_last_error_message ()).append (").") } ;
+        }
+        this->attach (h_new) ;
+#else	/* not (_WIN32 || _WIN64) */
+        int   fd = dup (h) ;
+        if (fd < 0) {
+            throw Error { ErrorCode::DUPLICATE_FAILED, "Duplicate handle failed." } ;
+        }
+        this->attach (fd) ;
+#endif	/* not (_WIN32 || _WIN64) */
+    }
+
+    Input & Input::open (const std::string &file) {
+#if defined (_WIN32) || defined (_WIN64)
+        HANDLE    H = CreateFile (file.c_str (), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0) ;
+        if (H == INVALID_HANDLE_VALUE) {
+            throw Error { ErrorCode::OPEN_FAILED
+                        , std::string { "Failed to open file [" }.append (file).append ("] (").append (get_last_error_message ()).append (").") } ;
+        }
+        h_.attach (static_cast <handle_t> (H)) ;
+#else	/* not (_WIN32 || _WIN64) */
+        int	fd = ::open (file.c_str (), O_RDONLY) ;
+        if (fd < 0) {
+            throw Error { ErrorCode::OPEN_FAILED
+                        , std::string { "Failed to open file [" }.append (file).append ("].") } ;
+        }
+        h_.attach (static_cast <handle_t> (fd)) ;
+#endif	/* not (_WIN32 || _WIN64) */
+        return *this ;
+    }
+
+    void	Output::open (const std::string &file) {
+#if defined (_WIN32) || defined (_WIN64)
+        HANDLE    H = CreateFile(file.c_str (), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0) ;
+        if (H == INVALID_HANDLE_VALUE) {
+            throw Error { Error::ERR_CREATE_FAILED
+                        , std::string { "Failed to create file [" }.append (file).append ("] (").append (get_last_error_message ()).append (").") } ;
+        }
+        h_.attach (static_cast <handle_t> (H)) ;
+#else	/* not (_WIN32 || _WIN64) */
+
+        int	fd = ::open (file.c_str (), O_CREAT | O_WRONLY | O_TRUNC, 0666) ;
+
+        if (fd < 0) {
+            throw Error (ErrorCode::CREATE_FAILED, std::string ("Failed to create file [").append (file).append ("].")) ;
+        }
+        h_.attach (static_cast <handle_t> (fd)) ;
+#endif	/* not (_WIN32 || _WIN64) */
+    }
+}
