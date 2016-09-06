2c95d9ed9cd7f8a3398dfdd7df7c9290685c119a
objectx <objectxtreme@gmail.com>
2016-09-06 17:46:33 +0900

    test: Add append mode tests

--- test/output.cpp
+++ test/output.cpp
@@ -24,14 +24,37 @@ TEST_CASE_METHOD (Fixture::DirectoryFixture, "Test: LowIO::Output", "[output]") 
     AND_WHEN ("Close should success") {
         auto cr = output.close () ;
         REQUIRE (!!cr) ;
-    AND_WHEN ("Read contents") {
-        std::ifstream   in { path.string (), std::ios::in | std::ios::binary } ;
-        REQUIRE (in.good ()) ;
-        char    tmp [64] ;
-        in.read (tmp, sizeof (tmp)) ;
-        auto sz = in.gcount () ;
-    THEN ("Contents should match") {
-        REQUIRE (sz == hello.size ()) ;
-        REQUIRE (memcmp (hello.data (), tmp, hello.size ()) == 0) ;
-    }}}}}}}}
+        AND_WHEN ("Read contents") {
+            std::ifstream   in { path.string (), std::ios::in | std::ios::binary } ;
+            REQUIRE (in.good ()) ;
+            char    tmp [64] ;
+            in.read (tmp, sizeof (tmp)) ;
+            auto sz = in.gcount () ;
+        THEN ("Contents should match") {
+            REQUIRE (sz == hello.size ()) ;
+            REQUIRE (memcmp (hello.data (), tmp, hello.size ()) == 0) ;
+        }}
+        AND_WHEN ("Open for append") {
+            LowIO::handle_t h { LowIO::open ( path.string ()
+                                            , ( LowIO::OpenFlags::CREATE
+                                              | LowIO::OpenFlags::APPEND
+                                              | LowIO::OpenFlags::READ_WRITE)
+                                            , 0666) } ;
+        THEN ("Should success") {
+            REQUIRE (h.valid ()) ;
+        AND_WHEN ("Append \"Hello World\".") {
+            REQUIRE (h.write (hello.c_str (), hello.size ())) ;
+            REQUIRE (h.close ()) ;
+        THEN ("File contains \"Hello World!HelloWorld!\"") {
+            std::ifstream in { path.string (), std::ios::in | std::ios::binary } ;
+            REQUIRE (in.good ()) ;
+            char    tmp [64] ;
+            in.read (tmp, sizeof (tmp)) ;
+            auto sz = in.gcount () ;
+            REQUIRE (sz == 2 * hello.size ()) ;
+            std::string actual { tmp, tmp + sz } ;
+            std::string expected { hello + hello } ;
+            REQUIRE (expected == actual) ;
+        }}}}
+    }}}}}}
 }
