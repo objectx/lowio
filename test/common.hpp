adc9192c307df2f5bfec155a90010f54735475cc
Masashi Fujita <objectxtreme@gmail.com>
2016-09-17 01:01:50 +0900

    chore: Use a manually maintained prefix header for `cotire`

---
+++ test/common.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Masashi Fujita on 2016/09/17.
+// Copyright (c) 2016 Polyphony Digital Inc. All rights reserved.
+//
+
+#pragma once
+#ifndef common_hpp__d5e0679b9db3416cba11dd99a5e52ca2
+#define common_hpp__d5e0679b9db3416cba11dd99a5e52ca2    1
+
+#include <stdint.h>
+#include <cache.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <fcntl.h>
+#include <boost/filesystem.hpp>
+
+#endif /* common_hpp__d5e0679b9db3416cba11dd99a5e52ca2 */
---
+++ test/pch.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Masashi Fujita on 2016/09/17.
+// Copyright (c) 2016 Polyphony Digital Inc. All rights reserved.
+//
+
+#include "common.hpp"
